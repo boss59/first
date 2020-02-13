c17f101d74b120ef8ebb58b4bfad5a32fceecd73
boss59 <2792593650@qq.com>
2020-02-13 18:00:45 +0800

    aa

--- session/mysql.php
+++ session/mysql.php
@@ -5,7 +5,6 @@ class  MyMysqlSession implements SessionHandlerInterface
 
     private $mysql;
     private $expire = 1440; // 设置时间
-    private $key_prefix = "php-mysql-session:";// key的前缀
 
     public function open($save_path, $session_id)
     {
@@ -17,19 +16,36 @@ class  MyMysqlSession implements SessionHandlerInterface
         return true;
     }
 
+    public function close()
+    {
+        // TODO: Implement close() method.
 
+        $this -> mysql -> close();
+        return true;
+    }
+
+    // 查询 数据库中的数据
     public function read($session_id)
     {
         // TODO: Implement read() method.
         // var_dump(__METHOD__);// 打印当前方法
         $select_sql = 'select * from t_session where session_id="'.$session_id.'"';
-
         $session_info = $this -> mysql -> query($select_sql) -> fetch_assoc();
-        if ($session_info){
 
+        if ($session_info)
+        {
             # 过期的session 不能被读取到
-            if (){
+            if ($session_info['expire'] < time()){
 
+                # 删除过期的session
+                $delete_sql = 'delete from t_session where session_id="'.$session_id.'"';
+                $delete_result = $this -> mysql -> query($delete_sql);
+
+                if ( $delete_result ){
+                    return serialize([]);
+                }else{
+                    return serialize([]);
+                }
             }else{
                 return $session_info['session_data'];
             }
@@ -38,28 +54,29 @@ class  MyMysqlSession implements SessionHandlerInterface
         }
     }
 
+    // 添加到 数据库中
     public function write($session_id, $session_data)
     {
         // TODO: Implement write() method.
         // var_dump($session_id,$session_data);
-
+        # 查询 session_id 是否 存在
         $sql_count = 'select * from t_session where session_id="'.$session_id.'"';
-
         $result = $this -> mysql -> query($sql_count) -> fetch_all(MYSQLI_ASSOC);
-        if (empty($result)){
 
-            $insert_sql = 'insert into t_session  values()';
-
+        if (empty($result))
+        {
+            $insert_sql = 'insert into t_session  values(NULL ,"'.$session_id.'","'.addslashes($session_data).'",'.(time()+$this -> expire).')';
             $insert = $this -> mysql -> query($insert_sql);
+
             if ($insert){
                 return true;
             }else{
                 return false;
             }
         }else{
-            $update_sql = 'update ';
-
+            $update_sql = 'update t_session set session_data="'.addslashes($session_data).'",expire='.(time() + $this ->expire).'where session_id="'.$session_id.'"';
             $update = $this -> mysql -> query($update_sql);
+
             if ($update){
                 return true;
             }else{
@@ -69,19 +86,12 @@ class  MyMysqlSession implements SessionHandlerInterface
 
     }
 
-    public function close()
-    {
-        // TODO: Implement close() method.
-
-        $this -> mysql -> close();
-        return true;
-    }
-
+    // 删除 当前 session_id 的数据
     public function destroy($session_id)
     {
         // TODO: Implement destroy() method.
 
-        $delete_sql = 'delete * from t_session where session_id="'.$session_id.'"';
+        $delete_sql = 'delete from t_session where session_id="'.$session_id.'"';
         $delete_result = $this -> mysql -> query($delete_sql);
 
         if ($delete_result){
@@ -92,14 +102,13 @@ class  MyMysqlSession implements SessionHandlerInterface
 
     }
 
+    // 全局 清理过期的session数据
     public function gc($maxlifetime)
     {
         // TODO: Implement gc() method.
-
-        # 手动清理过期的sesion，就不需要删除过期的key
-
-        $gc_sql = 'delete * from t_session where expire <'.time();
-
+//        var_dump('gc');
+        # 手动 清理过期的session数据，
+        $gc_sql = 'delete from t_session where expire <'.time();
         $gc_result = $this -> mysql -> query($gc_sql);
 
         if ($gc_result){
@@ -109,24 +118,30 @@ class  MyMysqlSession implements SessionHandlerInterface
         }
 
     }
+
 }
 
 
 $mysql_session  = new MyMysqlSession();
 
 session_set_save_handler(
-    [$redis_session,'open'],
-    [$redis_session,'close'],
-    [$redis_session,'read'],
-    [$redis_session,'write'],
-    [$redis_session,'destroy'],
-    [$redis_session,'gc']
+    [$mysql_session,'open'],
+    [$mysql_session,'close'],
+    [$mysql_session,'read'],
+    [$mysql_session,'write'],
+    [$mysql_session,'destroy'],
+    [$mysql_session,'gc']
 );
 
 session_start();
+//
+//$_SESSION['user_info'] = [
+//    'user_id' => 1,
+//    'user_name' => '阿斯顿撒'
+//];
 
-$_SESSION['user_info'] = [
-    'user_id' => 1,
-    'user_name' => '暗无天日'
-];
+// session_destroy();
+var_dump($_SESSION);
+
+echo "<hr />";
 ?>
