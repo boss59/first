de5c9146d8144918ba102654249946fcab3959f2
boss59 <2792593650@qq.com>
2020-02-20 18:25:26 +0800

    aa

--- table/order/goods_list.php
+++ table/order/goods_list.php
@@ -9,28 +9,40 @@
     $sql ='select * from shop_goods g LEFT JOIN shop_business b ON B.b_id = g.b_id';
 
     $goods_list = $mysql -> query($sql) -> fetch_all(MYSQLI_ASSOC);
-
-    echo "<style>td{padding: 5px 10px;}</style>";
-    echo '<table cellpadding="0" cellspacing="0" border="1">
-    <tr>
-           <tb>编号</tb>
-           <tb>商品名字</tb>
-           <tb>所属商家</tb>
-           <tb>价格</tb>
-           <tb>库存</tb>
-           <tb>操作</tb>
-    </tr></table>';
-
-    foreach( $goods_list as $k => $v){
-
-    echo '<tr>
-           <tb>'.$k.'</tb>
-           <tb>'.$v['goods_name'].'</tb>
-           <tb>'.$v['b_id'].'</tb>
-           <tb>'.$v['b_name'].'</tb>
-           <tb>'.$v['goods_price'].'</tb>
-           <tb>'.$v['stack'].'</tb> 
-           <tb><a href="./cart_add.php?goods_id='.$v['goods_id'].'&b_id='.$v['b_id'].'">加入购物车</a></tb>
-        </tr>';
-    }
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>商品列表</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr align="center">
+            <td>编号</td>
+            <td>商品名字</td>
+            <td>商家id</td>
+            <td>所属商家</td>
+            <td>价格</td>
+            <td>库存</td>
+            <td>操作</td>
+        </tr>
+    <?php foreach ( $goods_list as $k => $v){?>
+        <tr align="center">
+            <td><?php echo $k?></td>
+            <td><?php echo $v['goods_name']?></td>
+            <td><?php echo $v['b_id']?></td>
+            <td><?php echo $v['b_name']?></td>
+            <td><?php echo $v['goods_price']?></td>
+            <td><?php echo $v['stack']?></td>
+            <td>
+                <a href="./cart_add.php?goods_id=<?php echo $v['goods_id']?>&b_id=<?php echo $v['b_id']?>">加入购物车</a>
+            </td>
+        </tr>
+    <?php }?>
+    </table>
+</body>
+</html>
